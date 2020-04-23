6dc926723c767282500e1078244e8006398aeea6
Gusseth <charleskplotado@gmail.com>
2020-04-23 00:31:26 -0700

    More typesafe and now recognizes when the given arg has no numbers

--- Kawashiro Project/commands/modules/FlexmoneyCommand.cs
+++ Kawashiro Project/commands/modules/FlexmoneyCommand.cs
@@ -13,6 +13,8 @@ namespace Kawashiro_Project.commands.modules
 {
     public class FlexmoneyCommand : ModuleBase<SocketCommandContext>
     {
+        private SocketTextChannel channel;
+
         /// <summary>
         /// Makes the bot reply with the same message
         /// </summary>
@@ -22,11 +24,19 @@ namespace Kawashiro_Project.commands.modules
         [Summary("Flex on the bot.")]
         public async Task Flexmoney([Remainder] string yourMoney = "0")
         {
-            yourMoney.Replace(" ", string.Empty);
-            yourMoney.Replace(",", string.Empty);
-            yourMoney.Replace(".", string.Empty);
-            yourMoney = Regex.Replace(yourMoney, "[^0-9.-]", "");
-            BigInteger money = BigInteger.Parse(yourMoney);
+            channel = (Context.Channel as SocketTextChannel);
+            string yourMoneyParsed = yourMoney;
+
+            yourMoneyParsed.Replace(" ", string.Empty);
+            yourMoneyParsed.Replace(",", string.Empty);
+            yourMoneyParsed.Replace(".", string.Empty);
+            yourMoneyParsed = Regex.Replace(yourMoneyParsed, "[^0-9.-]", "");
+            if (string.IsNullOrEmpty(yourMoneyParsed))
+            {
+                await ReplyAsync(string.Format(LineManager.GetLine("FlexMoneyNoNumber"), yourMoney, Context.User.Mention));
+                return;
+            }
+            BigInteger money = BigInteger.Parse(yourMoneyParsed);
             if (money < 0)
             {
                 await ReplyAsync(string.Format(LineManager.GetLine("FlexMoneyBrokeBitch"), money, Context.User.Mention));
@@ -40,7 +50,15 @@ namespace Kawashiro_Project.commands.modules
             }
             catch (ArgumentException)
             {
-                await (Context.Channel as SocketTextChannel).SendFileAsync("data\\toomuchmoney.gif", null);
+                int randomChance = Nitori.random.Next(0, 1);
+                if (randomChance == 0)
+                {
+                    await channel.SendFileAsync("data\\jpow.gif", "My name is Jerome Powell of the US Federal Reserve and money printer goes __**BRRRRRRR**__");
+                }
+                else
+                {
+                    await channel.SendFileAsync("data\\toomuchmoney.gif", null);
+                }
             }
         }
     }
