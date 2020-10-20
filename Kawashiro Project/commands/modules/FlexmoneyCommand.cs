62a73dc670db76a559708f002637b1e33899fa1d
Gusseth <charleskplotado@gmail.com>
2020-10-19 23:31:26 -0700

    Documentation improvements

--- Kawashiro Project/commands/modules/FlexmoneyCommand.cs
+++ Kawashiro Project/commands/modules/FlexmoneyCommand.cs
@@ -16,9 +16,14 @@ namespace Kawashiro_Project.commands.modules
         private SocketTextChannel channel;
 
         /// <summary>
-        /// Makes the bot reply with the same message
+        /// Bot replies with your money + a random number between [1, int.MaxValue]
+        /// 
+        /// EXCEPT:
+        ///     - If there are no numbers,          then reply with FlexMoneyNoNumber
+        ///     - (BigInteger)yourMoney < 0         then reply with FlexMoneyBrokeBitch
+        ///     - Bot response goes over text limit then reply with a dated Jerome Powell joke
         /// </summary>
-        /// <param name="msg"></param>
+        /// <param name="yourMoney">Money amount.</param>
         /// <returns></returns>
         [Command("Flexmoney")]
         [Summary("Flex on the bot.")]
@@ -27,36 +32,59 @@ namespace Kawashiro_Project.commands.modules
             channel = (Context.Channel as SocketTextChannel);
             string yourMoneyParsed = yourMoney;
 
-            yourMoneyParsed.Replace(" ", string.Empty);
-            yourMoneyParsed.Replace(",", string.Empty);
-            yourMoneyParsed.Replace(".", string.Empty);
-            yourMoneyParsed = Regex.Replace(yourMoneyParsed, "[^0-9.-]", "");
-            if (string.IsNullOrEmpty(yourMoneyParsed))
+            yourMoneyParsed.Replace(" ", string.Empty); // Remove spaces - Looking back, this may be redundant
+            yourMoneyParsed.Replace(",", string.Empty); // Remove , - Looking back, this may be redundant
+            yourMoneyParsed.Replace(".", string.Empty); // Remove . - Looking back, this may be redundant
+            yourMoneyParsed = Regex.Replace(yourMoneyParsed, "[^0-9.-]", "");   // Remove non-numeric characters
+
+            // If there are no numbers in the message
+            if (string.IsNullOrEmpty(yourMoneyParsed))  
             {
-                await ReplyAsync(string.Format(ResponseManager.GetLine("FlexMoneyNoNumber"), yourMoney, Context.User.Mention));
+                await ReplyAsync(
+                    string.Format(ResponseManager.GetLine("FlexMoneyNoNumber"), // Load dialogue 
+                    yourMoney,                                                  // [0] = The message sent
+                    Context.User.Mention));                                     // [1] = The user who executed this command
                 return;
             }
-            BigInteger money = BigInteger.Parse(yourMoneyParsed);
+
+            // Convert to BigInteger because neither int nor int64 can help us here...
+            BigInteger money = BigInteger.Parse(yourMoneyParsed);   
+
+            // If the money is less than 0
             if (money < 0)
             {
-                await ReplyAsync(string.Format(ResponseManager.GetLine("FlexMoneyBrokeBitch"), money, Context.User.Mention));
+                await ReplyAsync(
+                    string.Format(ResponseManager.GetLine("FlexMoneyBrokeBitch"),  // Load dialogue 
+                    money,                                                         // [0] = The message sent
+                    Context.User.Mention));                                        // [1] = The user who executed this command
                 return;
             }
+
+            // Money added compared to user's amount
             uint addedAmount = (uint)Nitori.Random.Next(1, int.MaxValue);
 
             try
             {
-                await ReplyAsync(string.Format(ResponseManager.GetLine("FlexMoneyResponse"), money + addedAmount, money, addedAmount, Context.User.Mention));
+                // Normal case
+                await ReplyAsync(
+                    string.Format(ResponseManager.GetLine("FlexMoneyResponse"), // Load dialogue
+                    money + addedAmount,                                        // [0] = User's money + added amount, bot's money
+                    money,                                                      // [1] = User's money
+                    addedAmount,                                                // [2] = The added amount
+                    Context.User.Mention));                                     // [3] = The user who executed this command
             }
             catch (ArgumentException)
             {
-                int randomChance = Nitori.Random.Next(0, 1);
+                // Generic ArgumentException case, 99% of the time this is called when the bot's response exceeds the text limit
+                int randomChance = Nitori.Random.Next(0, 2);
                 if (randomChance == 0)
                 {
+                    // Random chance, 50/50
                     await channel.SendFileAsync("data\\jpow.gif", "My name is Jerome Powell of the US Federal Reserve and money printer goes __**BRRRRRRR**__");
                 }
                 else
                 {
+                    // Random chance, 50/50
                     await channel.SendFileAsync("data\\toomuchmoney.gif", null);
                 }
             }
